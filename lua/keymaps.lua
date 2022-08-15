29e3201503d1c1aa038431fcb9c0ab0c07c0c6c8
DusanDjordjic <dukidjordjic@gmail.com>
2022-08-15 21:07:31 +0300

    Keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,22 @@
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+keymap("n", "<S-k>", ":bnext<cr>", opts)
+keymap("n", "<S-j>", ":bprevious<cr>", opts)
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+keymap("v", "p", '"_dP', opts)
